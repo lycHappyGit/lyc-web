5afc5fca228523d9bd099af2a51a22f950bdc610
liuyingchao@mininglamp.com <liuyingchao@mininglamp.com>
2020-10-14 17:42:57 +0800

    项目结构修改

--- lyc-web2/src/main/java/cn/lyc/lycweb2/LycWeb2Application.java
+++ lyc-web2/src/main/java/cn/lyc/lycweb2/LycWeb2Application.java
@@ -2,12 +2,8 @@ package cn.lyc.lycweb2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
-@EnableEurekaClient
 public class LycWeb2Application {
 
     public static void main(String[] args) {
